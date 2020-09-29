01d8660436f1d524b1585f8032b899600965a51c
ericeschnei <ericeschnei@gmail.com>
2020-09-29 06:44:35 -0400

    bugfix3

--- PlayMode.cpp
+++ PlayMode.cpp
@@ -14,6 +14,7 @@
 #include <iostream>
 #include <ostream>
 #include <random>
+#include <ctime>
 
 GLuint meshes_for_lit_color_texture_program = 0;
 Load< MeshBuffer > meshes(LoadTagDefault, []() -> MeshBuffer const * {
